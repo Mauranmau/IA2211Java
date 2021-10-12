cae06a01ac3c2f4820e57598b550f7fb500c1779
Mauranmau <2019t00396@stu.cmb.ac.lk>
2021-10-12 13:46:17 +0530

    added example to main

--- IA2211LESSON/src/com/fotuoc/Main.java
+++ IA2211LESSON/src/com/fotuoc/Main.java
@@ -3,7 +3,7 @@ package com.fotuoc;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HALLO WORLD");
+        System.out.println("mauranmau");
 	// write your code here
     }
 }
